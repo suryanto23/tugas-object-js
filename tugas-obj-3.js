f4e89478d89870ae88031a17332a850d3dac7626
Suryanto <67571269+suryanto23@users.noreply.github.com>
2021-02-09 22:45:59 +0700

    menambahkan crud sebagai function

--- tugas-obj-3.js
+++ tugas-obj-3.js
@@ -3,33 +3,67 @@
 // Soal Nomor 3
 
 let todos = [
-    {id: 1, todo: "belajar coding"},
-    {id: 2, todo: "nanti tidur"}
-  ];
+  {id: 1, todo: "belajar coding"},
+  {id: 2, todo: "nanti tidur"}
+];
 
 
 //CRUD display printAll
+
+let printAll = () => {
+
 todos.forEach((i) => console.log(i.todo));
 
+};
+
+printAll();
+
+
+
 //CRUD display printByID
+
+let printByID = () => {
+
 console.log(todos[0].todo);
 
+};
+
+printByID();
+
+
+
 //CRUD add
+
+let add = () => {
+
 todos.push({id: 3, todo: "ngerjain tugas"});
 todos;
 
+};
+
+add();
+
+
 //CRUD deletebyID
+
+let deleteByID = () => {
+
 todos.splice(1,1);
 console.log(todos);
 
+};
+
+deleteByID();
+
+
 //CRUD updateByID
+
+let updateByID = () => {
+
 todos[0].todo = "belajar CRUD";
 todos[1].todo = "ngerjain TUGAS"
 console.log(todos);
 
+};
 
-
-  
-
-
-
+updateByID();
